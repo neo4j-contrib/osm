ddfc38c9f6e93436837b3ed37a7c0b6073e90d21
Craig Taverner <craig@amanzi.com>
2018-05-23 11:52:06 +0200

    Added skeleton classes and maven config

---
+++ src/main/java/org/neo4j/gis/osm/OSM.java
@@ -0,0 +1,9 @@
+package org.neo4j.gis.osm;
+
+public class OSM
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/org/neo4j/gis/osm/OSMTest.java
@@ -0,0 +1,15 @@
+package org.neo4j.gis.osm;
+
+import org.junit.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+
+public class OSMTest
+{
+    @Test
+    public void testOSM()
+    {
+        assertThat(true, equalTo( true ));
+    }
+}
